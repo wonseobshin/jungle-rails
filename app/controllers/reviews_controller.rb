f0b412691eb207fcd88e103c91c285226067f7a1
Won Seob Shin <wonseob07@gmail.com>
2019-07-15 20:34:18 +0000

    functionally good but needs styling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,11 @@
 class ReviewsController < ApplicationController
     def create
+        puts params
         @review = Review.new(review_params)
+        @review.product = Product.find_by(:id => params[:product_id])
+        @review.user = current_user
+        @review.save!
+        redirect_to product_path(params[:product_id])
     end
     
     private
